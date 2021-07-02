27f24fe268beb2f198585239500dade439493d69
Overnap <overnapworks@gmail.com>
2021-07-02 18:24:46 +0900

    Add Layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled"
+import React from "react"
+import Footer from "./Footer"
+import GlobalStyle from "./GlobalStyle"
+import Header from "./Header"
+
+const Main = styled.div`
+  margin: auto;
+  max-width: 960px;
+`
+
+interface Props {
+  children?: React.ReactNode
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <Main>{children}</Main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
