aa627d2edcf6d7652bfefe8295fd4aa027779ab6
Overnap <overnapworks@gmail.com>
2021-07-13 02:36:34 +0900

    Delete unnecessary tag nodes

--- src/gatsby/onCreateNode.ts
+++ src/gatsby/onCreateNode.ts
@@ -1,13 +1,10 @@
-import crypto from 'crypto'
 import { GatsbyNode } from "gatsby";
-import { createFilePath } from "gatsby-source-filesystem";
-import { v4 } from 'uuid'
+import { createFilePath } from "gatsby-source-filesystem"
 
 const onCreateNode: GatsbyNode['onCreateNode'] = ({
   node,
-  actions: { createNode, createNodeField },
-  getNode,
-  getNodesByType
+  actions: { createNodeField },
+  getNode
 }) => {
   if (node.internal.type === 'MarkdownRemark') {
     const pathName = createFilePath({ node, getNode, basePath: 'blog' })
@@ -18,24 +15,6 @@ const onCreateNode: GatsbyNode['onCreateNode'] = ({
       node,
       name: 'slug',
       value: pathName
-    })
-
-    // Create tag node for new one
-
-    const tagNodes = getNodesByType('Tag');
-    (node.frontmatter as any).tags.forEach((tag: string) => {
-      const isExists = tagNodes.find(n => n.name == tag)
-      if (!isExists) {
-        createNode({
-          name: tag,
-          id: v4(),
-          parent: getNodesByType('Site')[0].id, // Add a parent to avoid GC
-          internal: {
-            type: 'Tag',
-            contentDigest: crypto.createHash('md5').update(tag).digest('hex')
-          }
-        })
-      }
     })
   }
 }
