a75ee374af2f6b169ba6c56ff20152d137102db9
Overnap <overnapworks@gmail.com>
2021-07-19 10:13:06 +0900

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+
+const NotFound = () => {
+  return (
+    <>
+      <SEO title='Home' />
+      <Layout>
+        <h1>404 Page Not Found</h1>
+        <div>Sorry, the content you are looking for does not found.</div>
+      </Layout>
+    </>
+  )
+}
+
+export default NotFound
